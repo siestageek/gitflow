aa7a46b4020223be1d34c30f77bb84d095c151a9
siestageek <siestageek@gmail.com>
2024-02-15 09:12:50 +0900

    main.py 추가

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+from app.route.index import index_router
+
+app = FastAPI()
+
+# jinja2 설정
+templates = Jinja2Templates(directory='views/templates')
+app.mount('/static', StaticFiles(directory='views/static'), name='static')
+
+# 외부 route 파일 불러오기
+app.include_router(index_router)
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', reload=True)
