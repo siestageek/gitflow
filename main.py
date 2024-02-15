8927afecce6c76a210c052d137c76cf8c890de6b
Yunsung Lim <kwngjin@gmail.com>
2024-02-15 14:31:02 +0900

    main.py - 라우터 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from app.route.index import index_router
+from app.route.yunsung22 import yunsung22_router
 
 app = FastAPI()
 
@@ -12,6 +13,7 @@ app.mount('/static', StaticFiles(directory='views/static'), name='static')
 
 # 외부 route 파일 불러오기
 app.include_router(index_router)
+app.include_router(yunsung22_router, prefix='/yunsung_router')
 
 
 if __name__ == '__main__':
