39c396fe6afcfe3ee6c6f1d54ca1acd8774bb5ea
JuHyeon, Cho <jjh920@naver.com>
2024-02-15 14:15:40 +0900

    main.py 수정 - 라우터 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from app.route.index import index_router
+from app.route.jjh920 import jjh920_router
 
 app = FastAPI()
 
@@ -12,6 +13,7 @@ app.mount('/static', StaticFiles(directory='views/static'), name='static')
 
 # 외부 route 파일 불러오기
 app.include_router(index_router)
+app.include_router(jjh920_router, prefix='/jjh920')
 
 
 if __name__ == '__main__':
